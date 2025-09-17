1f16420a6db4dd0eecaaf597651d21e67bc4eebc
bibekshah220 <bibekshah425@gmail.com>
2025-09-17 23:46:27 +0545

    setup

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import "./App.css";
+import Profile from "./components/profile";
+
+function App() {
+  return (
+    <main>
+      <ul>
+        <li> !!!! html tages !!!!</li>
+        <li> css propeties</li>
+        <li>
+          {" "}
+          type of html tags[ semantic & non-semantic ][block elelment, inline,
+          inline block]
+        </li>
+        <li> jsx </li>
+        <li> funcational components </li>
+        <li>funcation name should be start with capital latters </li>
+        <li> must return single jsx element [wapped by a single tag ] </li>
+        <li> props</li>
+        <li> condition rendering </li>
+        <li> SPA & MPA </li>
+        <li>DOM </li>
+        <li> Virtual Dom </li>
+      </ul>
+      <Profile />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/profile.tsx
@@ -0,0 +1,10 @@
+const Profile = () => {
+  return (
+    <div>
+      <h1>User Name</h1>
+      <p> Name: Bibek Shah</p>
+      <p> Email: Bibek@gmail.com</p>
+    </div>
+  );
+};
+export default Profile;
