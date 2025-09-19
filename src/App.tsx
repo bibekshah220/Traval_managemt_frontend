5c12c966493851beca1b595c00eb82cd24f411b1
bibekshah220 <bibekshah425@gmail.com>
2025-09-19 19:20:45 +0545

    tailwindcss setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,10 @@
 import "./App.css";
-import Profile from "./components/profile";
 
 function App() {
   return (
     <main>
-      <ul>
-        <li> !!!! html tages !!!!</li>
-        <li> css propeties</li>
-        <li>
-          {" "}
-          type of html tags[ semantic & non-semantic ][block elelment, inline,
-          inline block]
-        </li>
-        <li> jsx </li>
-        <li> funcational components </li>
-        <li>funcation name should be start with capital latters </li>
-        <li> must return single jsx element [wapped by a single tag ] </li>
-        <li> props</li>
-        <li> condition rendering </li>
-        <li> SPA & MPA </li>
-        <li>DOM </li>
-        <li> Virtual Dom </li>
-
-        <ol>start with use</ol>
-        <p>eg</p>
-        <ol>
-          <li> usestate()</li>
-          <li> useeffect()</li>
-          <li> useref()</li>
-          <li> usememo()</li>
-          <li> usecallback()</li>
-        </ol>
-      </ul>
-      <Profile />
+      <h1 className="text-amber-200 text-4xl font-bold">Hello world</h1>
+      <h2>This is Traval mgmnt system</h2>
     </main>
   );
 }
